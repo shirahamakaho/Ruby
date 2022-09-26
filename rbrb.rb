ab475cd8b4a03aa4d0a2866d4ec8fbace1d8a42d
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-26 05:21:29 +0000

    ^^

--- rbrb.rb
+++ rbrb.rb
@@ -1,5 +1,52 @@
-gacha = {"奏汰"=>1,"千秋"=>50,"冬弥"=>1000,"彰人"=>5}
-
-gacha.each do |name,number|
-  puts "#{name}はガチャ#{number}回目で当たった！！"
+def rule(one)
+  if one == 1
+    "ガチマッチ"
+  elsif one == 2
+    "ナワバリバトル"
+  elsif one == 3
+    "サーモンラン"
+  elsif one == 4
+    "ヒーローモード"
+  elsif one == 5
+    "試し打ち場"
+  else
+    "現実世界"
+  end
 end
+
+def play(two)
+
+  if two == 1
+    "塗りまくら"
+  elsif two == 2
+    "キル取りまくら"
+  elsif two == 3
+    "コジャケとあそば"
+  elsif two == 4
+    "イクラ回収しまくら"
+  elsif two == 5
+    "メガホンライブし"
+  else
+    "勉強ガンバら"
+  end
+end
+
+def splatoon(ika,tako)
+  puts "#{ika}で#{tako}なイカ！？"
+end
+
+puts "ハッピー☆イカ占い"
+puts "1 から 5 の中から数字を 2つ 選ぼう！"
+puts "(同じ数字を選んでもいいぞ!)"
+
+puts "ひとつめは？"
+one = gets.to_i
+ ika = rule(one)
+
+puts "ふたつめは？"
+two = gets.to_i
+ tako = play(two)
+
+puts "今日は…"
+splatoon(ika,tako)
+puts "ほな カイサン!!!"
